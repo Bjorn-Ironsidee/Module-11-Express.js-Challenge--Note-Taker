77d616f02e331ce4bc24f99142c4c30cd48aba95
Bjorn-Ironsidee <109214871+Bjorn-Ironsidee@users.noreply.github.com>
2024-04-15 15:00:53 +1000

    final commit
    final commit bug fixes

--- server.js
+++ server.js
@@ -13,11 +13,11 @@ app.use(express.json());
 
 // Routes
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'notes.html'));
+    res.sendFile(path.join(__dirname, 'notes.html'));
 });
 
 app.get('/api/notes', (req, res) => {
@@ -44,6 +44,30 @@ app.post('/api/notes', (req, res) => {
       if (err) throw err;
       res.json(newNote);
     });
+  });
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  // Read the existing notes from db.json
+  fs.readFile(path.join(__dirname, 'db.json'), 'utf8', (err, data) => {
+    if (err) throw err;
+    const notes = JSON.parse(data);
+
+    // Find the index of the note with the given id
+    const index = notes.findIndex(note => note.id === parseInt(req.params.id));
+
+    // If the note is found, remove it from the notes array
+    if (index !== -1) {
+      notes.splice(index, 1);
+
+      // Write the updated notes array back to db.json
+      fs.writeFile(path.join(__dirname, 'db.json'), JSON.stringify(notes), (err) => {
+        if (err) throw err;
+        res.sendStatus(204); // Send a 204 (No Content) response
+      });
+    } else {
+      res.status(404).send('Note not found');
+    }
   });
 });
 
